a01c5da324a74a70c66c758cefca69b3678ebde7
Uffa Modey <83600229+UffaModey@users.noreply.github.com>
2021-06-07 06:32:37 +0100

    Robot motion test
    Robot motion in the following direction: right, left, forward, and back.

---
+++ motionTest.ino
@@ -0,0 +1,89 @@
+//www.elegoo.com
+
+
+#define ENA 5
+#define ENB 6
+#define IN1 7
+#define IN2 8
+#define IN3 9
+#define IN4 11
+
+#define carSpeed 100
+
+void right(){
+  analogWrite(ENA, carSpeed);
+  analogWrite(ENB, carSpeed);
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, LOW);
+  Serial.println("go forward!");
+}
+
+void left(){
+  analogWrite(ENA, carSpeed);
+  analogWrite(ENB, carSpeed);
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, HIGH);
+  Serial.println("go back!");
+}
+
+void forward(){
+  analogWrite(ENA, carSpeed);
+  analogWrite(ENB, carSpeed);
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+  Serial.println("go left!");
+}
+
+void back(){
+  analogWrite(ENA, carSpeed);
+  analogWrite(ENB, carSpeed);
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW); 
+  Serial.println("go right!");
+} 
+
+void stop(){
+   digitalWrite(ENA, LOW);
+   digitalWrite(ENB, LOW);
+   Serial.println("Stop!");
+} 
+
+void setup(){
+
+  pinMode(ENA, OUTPUT);
+  pinMode(ENB, OUTPUT);
+  pinMode(IN1,OUTPUT);
+  pinMode(IN2,OUTPUT);
+  pinMode(IN3,OUTPUT);
+  pinMode(IN4,OUTPUT);
+  
+  Serial.begin(9600);
+  
+}
+
+void loop() {
+  
+  right();
+  delay(1000);
+  left();
+  delay (1000);
+  forward();
+  delay(1000);
+  back();
+  delay(1000);
+  
+  stop();
+  delay (1000);
+
+  while(1 == 1){
+    
+    }
+}
